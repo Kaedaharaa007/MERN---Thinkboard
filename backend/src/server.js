a18b2bb9a5ad343d3147c2f5470b151c77312eb0
Richie <richieciangsawira@gmail.com>
2026-02-09 14:27:31 +0700

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"; //package.json type = "module" 
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"
 
 // const express = require("express"); //doesn't need type in package.json
 import notesRoutes from"./routes/notesRoutes.js"
@@ -13,15 +14,20 @@ dotenv.config(); //everytime we use env variable use dotenv.config
 
 const app = express();
 const PORT = process.env.PORT || 5001; //|| means if we cant get the variable, acsess 5001
+const __dirname = path.resolve()
 
 //nodemon -> !!MUST IN DEVELOPMENT!!
 //watch and update every single changes (no need to start server every changes)
 //node -> for deployment website
 
 //middle ware, dont forget next()
-app.use(cors({
-    origin:"http://localhost:5173",
-}))
+//allow frontend request to accsess api in backend
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }))
+}
+
 app.use(express.json());//parse our JSON bodies: req.body
 app.use(rateLimiter);
 //simple custom middleware
@@ -32,7 +38,14 @@ app.use(rateLimiter);
 
 //if there is api, direct to notesRoutes
 app.use("/api/notes", notesRoutes);
-//allow frontend request to accsess api in backend
+//backend serve our optimized react folder (dist)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.use((req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 //connect to mongoDB, our database
 connectDB().then(()=>{
